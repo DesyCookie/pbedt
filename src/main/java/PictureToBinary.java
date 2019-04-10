1e966a252cd2e17a52dd7e4d02848aa20285f6ca
Desy <desireenusch@gmail.com>
2019-04-10 14:09:45 +0200

    Detailed the TuplePoint Class

--- src/main/java/PictureToBinary.java
+++ src/main/java/PictureToBinary.java
@@ -2,29 +2,34 @@ import java.io.*;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.util.*;
+
+import static javax.imageio.ImageIO.read;
+
 public class PictureToBinary
 {
     public static void main(String args[]) throws IOException{
-        File file= new File("C:\\Users\\Desy\\Google Drive\\UNI\\FS19\\Algorithmen Projekt\\PBEDT\\src\\Test_row_1x100.jpg");
-        BufferedImage image = ImageIO.read(file);
+        String path = PictureToBinary.class.getResource("Test_row_1x100.jpg").getFile();
+        File file= new File(path);
+        BufferedImage image = read(file);
         Stack<TuplePoint> r = new Stack<>();
-        TuplePoint a = new TuplePoint();
+        Stack<TuplePoint> n = new Stack<>();
+
         // Getting pixel color by position x and y
         for(int y = 0; y <image.getHeight(); y++){
             for(int x = 0; x <image.getWidth(); x++){
         int clr=  image.getRGB(x,y);  // -1 = white, -16777216 = black
         if(clr == -1){
-            a.value = 1;
-            a.xcoordinate=x;
-            a.ycoordninate=y;
+            TuplePoint a = new TuplePoint(x,y,1);
+            n.push(a);
+            r.push(a);
         }
         else{
-            a.value=0;
-            a.xcoordinate=x;
-            a.ycoordninate=y;
+            TuplePoint a = new TuplePoint(x,y,0);
+            r.push(a);
         }
-        r.push(a);
+
     }
 }
-System.out.println(r);    }
+System.out.println(r);
+        System.out.println(n);    }
 }
