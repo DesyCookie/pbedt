e687e3f0b908201ba95da7b511d1d57b3d7a8758
Désy <desireenusch@gmail.com>
2019-05-20 15:00:12 +0200

    PBEDT 2D Implementation continued

--- src/main/java/PBEDT_2D.java
+++ src/main/java/PBEDT_2D.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class PBEDT_2D {
@@ -17,25 +18,25 @@ public class PBEDT_2D {
         }
     }
 
-    public static void SquareDistance2D(Stack<TuplePoint> points){
+    public static void SquareDistance2D(Stack<TuplePoint> points, int rows, int cols){
         int cx;
-        int m;
-        int n;
-        for(int r=0; r =m-1; r++){
-           TuplePoint[] stack_c;
-            TuplePoint[] stack_cx;
-            TuplePoint[] stack_g;
+        int m=rows; //rows
+        int n=cols;//collumns
+        for(int r=0; r ==m-1; r++){
+           ArrayList<TuplePoint> stack_c = new ArrayList<TuplePoint>();
+            ArrayList<TuplePoint>  stack_cx = new ArrayList<TuplePoint>();
+            ArrayList<TuplePoint>  stack_g = new ArrayList<TuplePoint>();
 
-           int p = -1;
-            for (int c=0;c=n-1;c++){
+           int p = -1;//point that is now being looked at
+            for (int c=0;c==n-1;c++){
                 if(points.size() < Integer.MAX_VALUE){
                     while(true){
                         if(p>=0){
-                            cx = Intersection_INT(stack_c[p].getXcoordinate(), c, stack_g[p].getXcoordinate(), points.size());
-                            if(cx == (stack_cx[p].getXcoordinate())){
+                            cx = Intersection_INT(stack_c.get(p).getXcoordinate(), c, stack_g.get(p).getXcoordinate(), points.size());
+                            if(cx == (stack_cx.get(p).getXcoordinate())){
                                 p=p-1;
                             }
-                                else if (cx < stack_cx[p].getXcoordinate()){
+                                else if (cx < stack_cx.get(p).getXcoordinate()){
                                     p = p-1;
                                     continue;
                                 }
@@ -46,9 +47,9 @@ public class PBEDT_2D {
                             else
                                 cx = -1;
                             p++;
-                            stack_c[p].setXcoordinate(c);
-                            stack_cx[p].setXcoordinate(cx);
-                            stack_g[p].setXcoordinate(points.size());
+                            stack_c.get(p).setXcoordinate(c);
+                            stack_cx.get(p).setXcoordinate(cx);
+                            stack_g.get(p).setXcoordinate(points.size()); // NOPE
                             break;
                         }
                     if(p<0){
@@ -60,7 +61,7 @@ public class PBEDT_2D {
                             cx = n-1;
                         }
                         else{
-                            cx=stack_cx[k+1].getXcoordinate();
+                            cx=stack_cx.get(k+1).getXcoordinate();
                         }
                         while(c<=cx){
                             //I(r,c)= Distance(c, stack_c[k].getXcoordinate(), stack_g[k].getXcoordinate())
